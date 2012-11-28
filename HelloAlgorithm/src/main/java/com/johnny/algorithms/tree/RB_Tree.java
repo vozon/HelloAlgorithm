57d44e5069a091303a99ce11b5c8e6bf864c85d7
vozon <who.love.me.god@gmail.com>
2012-11-29 00:36:11 +0800

    Initial

---
+++ HelloAlgorithm/src/main/java/com/johnny/algorithms/tree/RB_Tree.java
@@ -0,0 +1,13 @@
+package com.johnny.algorithms.tree;
+
+public class RB_Tree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
